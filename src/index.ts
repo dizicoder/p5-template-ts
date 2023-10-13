dda637dc2dc2bc5f41b70d555f52087cd9092575
dizicode <dizicode@MBP14.local>
2023-10-13 16:11:54 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as p5 from "p5";
+
+const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.draw = () => {
+    p.background(200);
+
+    p.circle(p.width / 2, p.height / 2, p.width / 4);
+  };
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+};
+
+new p5(sketch);
